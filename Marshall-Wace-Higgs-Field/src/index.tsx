00310cf76201eff123919b2c93b2350fca9af0eb
Traian-Caescu <32572689+Traian-Caescu@users.noreply.github.com>
2024-10-23 21:56:58 +0100

    Update index.tsx

--- Marshall-Wace-Higgs-Field/src/index.tsx
+++ Marshall-Wace-Higgs-Field/src/index.tsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { PrimeReactProvider } from 'primereact/api';
-import "primereact/resources/themes/lara-light-cyan/theme.css";
-
+import "primereact/resources/themes/lara-light-cyan/theme.css"; 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
